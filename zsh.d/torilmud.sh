c0a9a25fcff353b90321fb953f328b39cc2df8bf
Lucifer Avada <lucifer.avada@gmail.com>
2017-07-23 00:53:15 -0400

    add action, alias and rogue tintin configuration files

--- zsh.d/torilmud.sh
+++ zsh.d/torilmud.sh
@@ -1,12 +1,10 @@
 ## Toril MUD
 #  Variables
-local TORILMUD_HOME="${HOME}/.torilmud"
+local TORILMUD="${HOME}/.torilmud"
 
 
 #  Login to Toril MUD
 function torilmud() {
-  local CONFIG="${TORILMUD_HOME}/main.tt"
-  echo "config: ${CONFIG}"
-  local SESSION="#session toril torilmud.org 9999"
-  tintin -G -r "${CONFIG}" -e "${SESSION}" ${@}
+  local CONFIG="${TORILMUD}/main.tt"
+  tintin -G -r "${CONFIG}" ${@}
 }
