14a9b23bdcbfc6370896d72ea2c6c735f7dc3f9b
Lucifer Avada <lucifer.avada@gmail.com>
2017-07-23 01:11:03 -0400

    move main.tt to torilmudrc and torilmud to torilmud.d

--- zsh.d/torilmud.sh
+++ zsh.d/torilmud.sh
@@ -1,10 +1,6 @@
 ## Toril MUD
-#  Variables
-local TORILMUD="${HOME}/.torilmud"
-
-
 #  Login to Toril MUD
 function torilmud() {
-  local CONFIG="${TORILMUD}/main.tt"
-  tintin -G -r "${CONFIG}" ${@}
+  local TORILMUD="${HOME}/.torilmudrc"
+  tintin -G -r "${TORILMUD}" ${@}
 }
