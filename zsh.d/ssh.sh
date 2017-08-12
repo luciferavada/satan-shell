28464f0fbe503d2b7a4436e13ca97caef6b7b32a
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-12 15:56:14 -0400

    add ssh key functions and add ssh keys to ssh-agent

--- zsh.d/ssh.sh
+++ zsh.d/ssh.sh
@@ -2,5 +2,27 @@
 #  SSH key files
 local SSH_KEYS=("luciferavada")
 
-#  Add sugarush ssh key to ssh-agent
-eval $(/usr/local/bin/keychain --eval -q ${SSH_KEYS[@]})
+#  SSH key directory
+local SSH_DIR="${HOME}/.ssh"
+
+#  List SSH keys in the SSH agent
+function ssh-list-keys() {
+  ssh-add -l
+}
+
+#  Find key in the SSH agent
+function ssh-find-key() {
+  ssh-list-keys | grep "${1}"
+}
+
+#  Add an SSH key to the SSH agent
+function ssh-add-key() {
+  ssh-add "${SSH_DIR}/${1}"
+}
+
+#  Add SSH keys to the SSH agent
+for key in ${SSH_KEYS}; do
+  if [ -z "$(ssh-find-key ${key})" ]; then
+    ssh-add-key "${key}"
+  fi
+done
