9c11e279f29a7939d8089edda987d7ee93a0c8f8
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-13 15:29:56 -0400

    remove autoenv dependency and add unit testing functions

--- zsh.d/python.sh
+++ zsh.d/python.sh
@@ -11,22 +11,21 @@ function pip-clean() {
 }
 
 
-## Python AutoEnv
+## Python Virtual Environment
+#  Virtual environment folder
+local VENV_FOLDER="venv"
+
+#  Virtual environment activation file
+local VENV_ACTIVATE=".venv-activate"
+
+#  Virtual environment deactivation file
+local VENV_DEACTIVATE=".venv-deactivate"
+
 #  Requirements Filename
 local REQUIREMENTS_FILE=".requirements"
 
-#  AutoEnv file
-local AUTOENV_ENV_FILENAME=".pythonrc"
-
-#  AutoEnv path
-local AUTOENV_PATH="$(brew --prefix autoenv)"
-
-#  Source AutoEnv shell script
-source "${AUTOENV_PATH}/activate.sh"
-
 #  Initialize a Python virtual environment
 function venv-init() {
-  local VENV_FOLDER="venv"
   local PYTHON="${1:-python2.7}"
   local PYTHON_PATH=$(which ${PYTHON})
 
@@ -34,24 +33,30 @@ function venv-init() {
     virtualenv --python "${PYTHON_PATH}" "${VENV_FOLDER}"
   fi
 
-  if [ ! -f "${AUTOENV_ENV_FILENAME}" ]; then
-    echo "source \"\${PWD}/venv/bin/activate\"" > "${AUTOENV_ENV_FILENAME}"
+  if [ ! -f "${VENV_ACTIVATE}" ]; then
+    echo "export MODULE=\"\"" > "${VENV_ACTIVATE}"
+  fi
+
+  if [ ! -f "${VENV_DEACTIVATE}" ]; then
+    echo "unset MODULE" > "${VENV_DEACTIVATE}"
   fi
 
   # activate the newly created python virtual environment
   venv-activate
-  # write 'y' to stdin to enter the virtual environment for the first time
-  cat <<< "y" 2>&1 /dev/null
 }
 
 #  Activate a python virtual environment
 function venv-activate() {
-  # read the .pythonrc file in the current directory
-  cd .
+  if [ -n "${VIRTUAL_ENV}" ]; then
+    venv-deactivate
+  fi
+  source "${PWD}/${VENV_FOLDER}/bin/activate"
+  source "${PWD}/${VENV_ACTIVATE}"
 }
 
 #  Deactivate a python virtual environment
 function venv-deactivate() {
+  source "$(dirname ${VIRTUAL_ENV})/${VENV_DEACTIVATE}"
   deactivate
 }
 
@@ -70,7 +75,7 @@ function venv-update() {
 
 #  Remove packages from the virtual environment
 function venv-clean() {
-  if [ -f "${AUTOENV_ENV_FILENAME}" ]; then
+  if [ -f "${VENV_ACTIVATE}" ]; then
     venv-activate
     pip-clean
     venv-update
@@ -83,5 +88,25 @@ function venv-clean() {
 function venv-name() {
   if [ -n "${VIRTUAL_ENV}" ]; then
     basename $(dirname "${VIRTUAL_ENV}")
+  fi
+}
+
+function venv-python-test() {
+  if [ $(which -s coverage) ]; then
+    coverage run --source "${MODULE}" \
+      --module "unittest" "test"
+    coverage report
+  else
+    python -m "unittest" "test"
+  fi
+}
+
+function venv-tornado-test() {
+  if [ $(which -s coverage) ]; then
+    coverage run --source "${MODULE}" \
+      --module "tornado.testing" "test"
+    coverage report
+  else
+    python -m "tornado.testing" "test"
   fi
 }
