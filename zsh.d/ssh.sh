0cd40040c394d08d9bc91f0e1a47a373cfd5e900
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-12 21:17:10 -0400

    add linux support

--- zsh.d/ssh.sh
+++ zsh.d/ssh.sh
@@ -5,6 +5,9 @@ local SSH_KEYS=("luciferavada")
 #  SSH key directory
 local SSH_DIR="${HOME}/.ssh"
 
+#  SSH agent variables
+local SSH_AGENT="${SSH_DIR}/.ssh-agent"
+
 #  List SSH keys in the SSH agent
 function ssh-list-keys() {
   ssh-add -l
@@ -12,13 +15,22 @@ function ssh-list-keys() {
 
 #  Find key in the SSH agent
 function ssh-find-key() {
-  ssh-list-keys | grep "${1}"
+  ssh-list-keys | grep --only-matching --regexp "${SSH_DIR}/${1}"
 }
 
 #  Add an SSH key to the SSH agent
 function ssh-add-key() {
   ssh-add "${SSH_DIR}/${1}"
 }
+
+#  Start an ssh-agent
+#  XXX: untested
+if [ ! "$(uname)" == "Darwin" ]; then
+  if [ ! -f "${SSH_AGENT}" ]; then
+    ssh-agent > "${SSH_AGENT}"
+  fi
+  eval "$(cat ${SSH_AGENT})"
+fi
 
 #  Add SSH keys to the SSH agent
 for key in ${SSH_KEYS}; do
