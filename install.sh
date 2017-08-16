3e6889d55fefd5d16ca13ae25200d780fb112ef7
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-15 21:27:11 -0400

    fix comments

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env zsh
 
-# skip the following files when installing
-local SKIP=("install.sh" "README.md")
+#  Skip the following files when installing
+local SKIP=("install.sh" "github.sh" "README.md")
 
-# split paths on `/` and store in an array
+#  Split paths on `/` and store in an array
 local HOME_PATH=(`echo ${HOME//\// }`)
 local BASE_PATH=(`echo ${PWD//\// }`)
 
-# remove ${HOME_PATH} from ${BASE_PATH} to create ${BASE}
+#  Remove ${HOME_PATH} from ${BASE_PATH} to create ${BASE}
 for i in ${HOME_PATH}; do
   # remove any element which contains $i
   local BASE_PATH=(${BASE_PATH[@]//*$i*})
 done
 
-# join ${BASE_PATH} with `/` and store as a string
+#  Join ${BASE_PATH} with `/` and store as a string
 local BASE="${BASE_PATH// //}"
 
-# create an array from files in the current directory
+#  Create an array from files in the current directory
 local FILES=(*)
 
 if [ -n "${FILES}" ]; then
@@ -29,8 +29,8 @@ if [ -n "${FILES}" ]; then
     local SRC="${BASE}/${file}"
     local DST="${HOME}/.${file}"
 
-    ln -sfh "${SRC}" "${DST}"
     echo "linking: ${SRC} -> ${DST}"
+    ln -sfh "${SRC}" "${DST}"
 
   done
 fi
