741b21c56ca170a7b94f9ae7aa262e6901e579c8
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-18 12:39:54 -0400

    simplify base path creation

--- install.sh
+++ install.sh
@@ -1,45 +1,24 @@
 #!/usr/bin/env zsh
 
-#  Source utility file
-source "${PWD}/util/util.sh"
-
-#  Skip the following files and directories when installing
-local SKIP=(
-  "install.sh" "install-github.sh" "README.md" "util" "zlogin.example"
+#  Install files
+local INSTALL_FILES=(
+  "zshenv" "zprofile" "zshrc"
+  "zsh.d" "zsh.d.conf" "zsh.d.modules"
 )
 
-#  Split paths on `/` and store in an array
-local HOME_PATH=(`echo ${HOME//\// }`)
-local BASE_PATH=(`echo ${PWD//\// }`)
-
-#  Remove ${HOME_PATH} from ${BASE_PATH} to create ${BASE}
-for i in ${HOME_PATH}; do
-  # remove any element which contains $i
-  local BASE_PATH=(${BASE_PATH[@]//*$i*})
-done
-
-#  Join ${BASE_PATH} with `/` and store as a string
-local BASE="${BASE_PATH// //}"
-
-#  Create an array from files in the current directory
-local FILES=(*)
+#  Link source path
+local BASE="${PWD#${HOME}/}"
 
 #  Link files
-if [ -n "${FILES}" ]; then
-  for file in ${FILES[@]}; do
+for file in ${INSTALL_FILES[@]}; do
 
-    if [ $(contains "${file}" "${SKIP[@]}") ]; then
-      continue
-    fi
+  local SRC="${BASE}/${file}"
+  local DST="${HOME}/.${file}"
 
-    local SRC="${BASE}/${file}"
-    local DST="${HOME}/.${file}"
+  echo "linking: ${SRC} -> ${DST}"
+  ln -sfh "${SRC}" "${DST}"
 
-    echo "linking: ${SRC} -> ${DST}"
-    #ln -sfh "${SRC}" "${DST}"
-
-  done
-fi
+done
 
 #  Modules file
 local ZSHELL_MODULES_FILE="${HOME}/.zsh.d/modules.conf"
@@ -48,10 +27,9 @@ local ZSHELL_MODULES_FILE="${HOME}/.zsh.d/modules.conf"
 if [ ! -f "${ZSHELL_MODULES_FILE}" ]; then
   echo "#  Modules are loaded in order" > "${ZSHELL_MODULES_FILE}"
   echo "MODULES=(" >> "${ZSHELL_MODULES_FILE}"
-  echo "  \"prompt\" \"history\" \"ls\" \"man\" \"ssh\" \"git\"" >> \
+  echo "  \"prompt\" \"history\" \"man\" \"ls\" \"git\"" >> \
     "${ZSHELL_MODULES_FILE}"
   echo ")" >> "${ZSHELL_MODULES_FILE}"
-  echo "\n" >> "${ZSHELL_MODULES_FILE}"
 fi
 
 #  RC file
@@ -60,18 +38,17 @@ local ZSHELL_RC_FILE="${HOME}/.zsh.d/rc.conf"
 #  Write default rc file
 if [ ! -f "${ZSHELL_RC_FILE}" ]; then
   echo "#  Install dircetory" > "${ZSHELL_RC_FILE}"
-  echo "ZSHELL_INSTALL_DIRECTORY=\"${BASE}\"" >> "${ZSHELL_RC_FILE}"
-  echo "\n" >> "${ZSHELL_RC_FILE}"
+  echo "ZSHELL_INSTALL_DIRECTORY=\"${PWD}\"" >> "${ZSHELL_RC_FILE}"
+  echo "" >> "${ZSHELL_RC_FILE}"
 
   echo "#  Configuration directory" >> "${ZSHELL_RC_FILE}"
   echo "ZSHELL_CONFIGURATION_DIRECTORY=\"${HOME}/.zsh.d.conf\"" >> \
     "${ZSHELL_RC_FILE}"
-  echo "\n" >> "${ZSHELL_RC_FILE}"
+  echo "" >> "${ZSHELL_RC_FILE}"
 
   echo "#  Modules directory" >> "${ZSHELL_RC_FILE}"
   echo "ZSHELL_MODULES_DIRECTORY=\"${HOME}/.zsh.d.modules\"" >> \
     "${ZSHELL_RC_FILE}"
-  echo "\n" >> "${ZSHELL_RC_FILE}"
 fi
 
 #  Create zlogin file
