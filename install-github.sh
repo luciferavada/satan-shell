1a1b84a19160f11e5c5c40a3a5f43017f9f296d8
Lucifer Avada <lucifer.avada@gmail.com>
2017-08-19 00:27:57 -0400

    update repository name

--- install-github.sh
+++ install-github.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 #  Github repository url
-local GITHUB_URL="https://github.com/luciferavada/zshell-configuration.git"
+local GITHUB_URL="https://github.com/luciferavada/satan-shell.git"
 
 #  Git clone destination directory
-local INSTALL_DIRECTORY="${HOME}/.zshell-configuration"
+local INSTALL_DIRECTORY="${HOME}/.satan-shell"
 
 #  Clone the repository
 git clone "${GITHUB_URL}" "${INSTALL_DIRECTORY}"
