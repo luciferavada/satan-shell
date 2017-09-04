d414c0dd5c8791564a28faadb3a86a47ce833fc6
Lucifer Avada <lucifer.avada@gmail.com>
2017-09-04 17:54:58 -0400

    use satan install directory

--- ascii.sh
+++ ascii.sh
@@ -4,13 +4,13 @@
 echo "$(tput sgr0; tput bold; tput setaf 0)"
 
 #  Display ascii-art
-cat "${PWD}/ascii-art"
+cat "${SATAN_INSTALL_DIRECTORY}/ascii-art"
 
 #  Set color to bold red
 echo "$(tput bold; tput setaf 1)"
 
 #  Display ascii-title
-cat "${PWD}/ascii-title"
+cat "${SATAN_INSTALL_DIRECTORY}/ascii-title"
 
 #  Reset display colors
 echo "$(tput setaf sgr0)"
