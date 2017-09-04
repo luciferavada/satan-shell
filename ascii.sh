7829bb6433995c6fafdd4f93a4c8301de6564171
Lucifer Avada <lucifer.avada@gmail.com>
2017-09-04 16:19:22 -0400

    use color codes instead of variables

--- ascii.sh
+++ ascii.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env zsh
 
 #  Set color to red
-echo -n "$(tput ${COLOR[reset]}; tput setaf ${COLOR[red]})\n"
+echo -n "$(tput sgr0; tput bold; tput setaf 0)\n"
 
 #  Display ascii-art
 cat "${PWD}/ascii-art"
 
 #  Set color to bold red
-echo -n "$(tput bold; tput setaf ${COLOR[red]})\n"
+echo -n "$(tput bold; tput setaf 1)\n"
 
 #  Display ascii-title
 cat "${PWD}/ascii-title"
 
 #  Reset display colors
-echo "$(tput setaf ${COLOR[reset]})"
+echo "$(tput setaf sgr0)"
