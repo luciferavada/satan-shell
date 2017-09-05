61d543e285d306a3fc7809cec1dbf16466d73407
Lucifer Avada <lucifer.avada@gmail.com>
2017-09-05 10:51:49 -0400

    update variable names and gitkeep file removal

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 #  Install files
 local SATAN_FILES=(
   "zshenv" "zprofile" "zshrc" "zlogin"
+)
+
+#  Install directories
+local SATAN_DIRECTORIES=(
   "zsh.d" "zsh.d.conf" "zsh.d.modules"
 )
 
 #  Files to backup
-local SATAN_FILES_BACKUPS=()
+local SATAN_BACKUPS=()
 
 #  Files to link
-local SATAN_FILES_LINKS=()
+local SATAN_LINKS=()
 
-#  Link source path
-local SATAN_SHELL_SOURCE="${PWD#${HOME}/}"
+#  Gitkeep files
+local SATAN_GITKEEP_FILES=()
 
 #  Modules file
 local SATAN_MODULES_FILE="${HOME}/.zsh.d/modules.conf"
@@ -27,29 +31,32 @@ local SATAN_VARIABLES_FILE="${HOME}/.zsh.d/variables.conf"
 #  Zlogin file
 local SATAN_ZLOGIN_FILE="${HOME}/.zlogin"
 
+#  Link source path
+local SATAN_SHELL_SOURCE="${PWD#${HOME}/}"
+
 #  Date stamp
 local DATE_STAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 #  Check for files to backup
-for file in ${SATAN_FILES[@]}; do
+for file in ${SATAN_FILES[@]} ${SATAN_DIRECTORIES[@]}; do
 
   local FILE="${HOME}/.${file}"
 
   #  Don't backup links
   if [ -f "${FILE}" ] && [ ! -L "${FILE}" ]; then
-    SATAN_FILES_BACKUPS+=("${file}")
+    SATAN_BACKUPS+=("${file}")
   fi
 
 done
 
 #  Backup files
-if [ -n "${SATAN_FILES_BACKUPS}" ]; then
+if [ -n "${SATAN_BACKUPS}" ]; then
   #  Colorized output
   echo -n "$(tput bold; tput setaf 2)"
   echo "--> Backing up files..."
   echo -n "$(tput sgr0)"
 
-  for file in ${SATAN_FILES_BACKUPS[@]}; do
+  for file in ${SATAN_BACKUPS[@]}; do
 
     local SRC="${HOME}/.${file}"
     local DST="${HOME}/.${file}.${DATE_STAMP}"
@@ -61,7 +68,7 @@ if [ -n "${SATAN_FILES_BACKUPS}" ]; then
 fi
 
 #  Check for files to link
-for file in ${SATAN_FILES[@]}; do
+for file in ${SATAN_FILES[@]} ${SATAN_DIRECTORIES[@]}; do
 
   #  Don't link zlogin
   if [ "${file}" = "zlogin" ]; then
@@ -72,19 +79,19 @@ for file in ${SATAN_FILES[@]}; do
   local DST="${HOME}/.${file}"
 
   if [ ! "$(readlink ${DST})" = "${SRC}" ]; then
-    SATAN_FILES_LINKS+=("${file}")
+    SATAN_LINKS+=("${file}")
   fi
 
 done
 
 #  Link files
-if [ -n "${SATAN_FILES_LINKS}" ]; then
+if [ -n "${SATAN_LINKS}" ]; then
   #  Colorized output
   echo -n "$(tput bold; tput setaf 2)"
   echo "--> Linking files..."
   echo -n "$(tput sgr0)"
 
-  for file in ${SATAN_FILES_LINKS[@]}; do
+  for file in ${SATAN_LINKS[@]}; do
 
     local SRC="${SATAN_SHELL_SOURCE}/${file}"
     local DST="${HOME}/.${file}"
@@ -92,6 +99,28 @@ if [ -n "${SATAN_FILES_LINKS}" ]; then
     echo "${SRC} -> ${DST}"
     ln -sfh "${SRC}" "${DST}"
 
+  done
+fi
+
+#  Check for gitkeep files to remove
+for directory in ${SATAN_DIRECTORIES[@]}; do
+  local GITKEEP_FILE="${PWD}/${directory}/.gitkeep"
+  if [ -f "${GITKEEP_FILE}" ]; then
+    SATAN_GITKEEP_FILES+=(${GITKEEP_FILE})
+  fi
+done
+
+#  Remove gitkeep files
+if [ -n "${SATAN_GITKEEP_FILES}" ]; then
+  #  Colorized output
+  echo -n "$(tput bold; tput setaf 2)"
+  echo "--> Removing .gitkeep files..."
+  echo -n "$(tput sgr0)"
+
+  for file in ${SATAN_GITKEEP_FILES[@]}; do
+    echo "${file}"
+    git update-index --assume-unchanged "${file}"
+    rm -f "${file}"
   done
 fi
 
@@ -176,17 +205,6 @@ if [ ! -f "${SATAN_ZLOGIN_FILE}" ]; then
   echo "#  Display ascii title" >> "${SATAN_ZLOGIN_FILE}"
   echo "#satan-ascii-title" >> "${SATAN_ZLOGIN_FILE}"
 fi
-
-#  Remove and ignore gitkeep files
-for files in ${SATAN_FILES[@]}; do
-  if [ -d "${PWD}/${file}" ]; then
-    local GITKEEP="${PWD}/${file}/.gitkeep"
-    if [ -f "${GITKEEP}" ]; then
-      git update-index --assume-unchanged "${GITKEEP}"
-      rm -f "${GITKEEP}"
-    fi
-  fi
-done
 
 #  Load the environment
 source "${HOME}/.zshenv"
