6f7576fe67bebd5f95a3f6ea6786d382b60108fa
Lucifer Avada <lucifer.avada@gmail.com>
2017-09-05 10:32:17 -0400

    use current working dircectory instead of home directory

--- install.sh
+++ install.sh
@@ -179,8 +179,8 @@ fi
 
 #  Remove and ignore gitkeep files
 for files in ${SATAN_FILES[@]}; do
-  if [ -d "${file}" ]; then
-    local GITKEEP="${HOME}/.${file}/.gitkeep"
+  if [ -d "${PWD}/${file}" ]; then
+    local GITKEEP="${PWD}/.${file}/.gitkeep"
     if [ -f "${GITKEEP}" ]; then
       git update-index --assume-unchanged "${GITKEEP}"
       rm -f "${GITKEEP}"
