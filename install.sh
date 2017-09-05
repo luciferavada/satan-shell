8f82f3a12b86efeebabc10307d3855e0ec53a3b8
Lucifer Avada <lucifer.avada@gmail.com>
2017-09-04 22:56:43 -0400

    backup zlogin with a datestamp

--- install.sh
+++ install.sh
@@ -17,27 +17,6 @@ local SATAN="${PWD#${HOME}/}"
 
 #  Colorized output
 echo -n "$(tput bold; tput setaf 2)"
-echo "--> Backing up files..."
-echo -n "$(tput sgr0)"
-
-#  Date stamp
-local DATE_STAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-
-#  Backup files
-for file in ${SATAN_FILES[@]}; do
-
-  local SRC="${SATAN}/${file}"
-  local DST="${HOME}/.${file}"
-
-  if [ -f "${DST}" ]; then
-    echo "${SRC} -> ${DST}.${DATE_STAMP}"
-    mv "${DST}" "${DST}.${DATE_STAMP}"
-  fi
-
-done
-
-#  Colorized output
-echo -n "$(tput bold; tput setaf 2)"
 echo "--> Linking files..."
 echo -n "$(tput sgr0)"
 
@@ -54,6 +33,12 @@ done
 
 #  Write default rc file
 if [ ! -f "${SATAN_RC}" ]; then
+  #  Colorized output
+  echo -n "$(tput bold; tput setaf 2)"
+  echo "--> Writing default ~/.zsh.d/rc.conf..."
+  echo -n "$(tput sgr0)"
+
+  #  Write file
   echo "#  Install dircetory" > "${SATAN_RC}"
   echo "SATAN_INSTALL_DIRECTORY=\"${PWD}\"" >> "${SATAN_RC}"
   echo "" >> "${SATAN_RC}"
@@ -71,11 +56,16 @@ if [ ! -f "${SATAN_RC}" ]; then
   echo "SATAN_REPOSITORIES=(" >> "${SATAN_RC}"
   echo "  \"satan-core\" \"satan-extra\" \"satan-community\"" >> "${SATAN_RC}"
   echo ")" >> "${SATAN_RC}"
-  echo "" >> "${SATAN_RC}"
 fi
 
 #  Write default modules file
 if [ ! -f "${SATAN_MODULES}" ]; then
+  #  Colorized output
+  echo -n "$(tput bold; tput setaf 2)"
+  echo "--> Writing default ~/.zsh.d/modules.conf..."
+  echo -n "$(tput sgr0)"
+
+  #  Write file
   echo "#  Modules are loaded in order" > "${SATAN_MODULES}"
   echo "SATAN_MODULES=(" >> "${SATAN_MODULES}"
   echo "  \"prompt\" \"history\" \"man\" \"ls\" \"git\"" >> \
@@ -83,19 +73,44 @@ if [ ! -f "${SATAN_MODULES}" ]; then
   echo ")" >> "${SATAN_MODULES}"
 fi
 
+#  Backup zlogin
+if [ -f "${HOME}/.zlogin" ]; then
+  #  Colorized output
+  echo -n "$(tput bold; tput setaf 2)"
+  echo "--> Backing up ~/.zlogin..."
+  echo -n "$(tput sgr0)"
+
+  #  Date stamp
+  local DATE_STAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+  #  Add datestamp to old zlogin
+  echo "~/.zlogin -> ~/.zlogin.${DATE_STAMP}"
+  mv "${HOME}/.zlogin" "${HOME}/.zlogin.${DATE_STAMP}"
+fi
+
 #  Create zlogin file
 if [ ! -f "${HOME}/.zlogin" ]; then
+  #  Colorized output
+  echo -n "$(tput bold; tput setaf 2)"
+  echo "--> Writing default ~/.zlogin..."
+  echo -n "$(tput sgr0)"
+
+  #  Write file
   echo "## User customization goes here" > "${HOME}/.zlogin"
   echo "" >> "${HOME}/.zlogin"
+
   echo "#  Display ascii artwork with title and credit" >> "${HOME}/.zlogin"
   echo "#satan-ascii-header" >> "${HOME}/.zlogin"
   echo "" >> "${HOME}/.zlogin"
+
   echo "#  Display ascii artwork" >> "${HOME}/.zlogin"
   echo "#satan-ascii-art" >> "${HOME}/.zlogin"
   echo "" >> "${HOME}/.zlogin"
+
   echo "#  Display credit" >> "${HOME}/.zlogin"
   echo "satan-credit" >> "${HOME}/.zlogin"
   echo "" >> "${HOME}/.zlogin"
+
   echo "#  Display ascii title" >> "${HOME}/.zlogin"
   echo "satan-ascii-title" >> "${HOME}/.zlogin"
 fi
@@ -105,9 +120,10 @@ source "${HOME}/.zshenv"
 source "${HOME}/.zprofile"
 source "${HOME}/.zshrc"
 
-#  Display art and credit
+#  Display ascii art
 satan-ascii-art
 
+#  Load zlogin
 source "${HOME}/.zlogin"
 
 #  Move to the home directory
