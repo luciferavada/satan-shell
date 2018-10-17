a6a787edca2c6bbf7aa39c20cd64ff6100f4fa99
Paul Severance <paul.severance@gmail.com>
2018-10-16 21:14:33 -0400

    fix install url

--- install-github.sh
+++ install-github.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 #  Github repository url
-local GITHUB_URL="https://github.com/satanush/satan-shell.git"
+local GITHUB_URL="https://github.com/luciferavada/satan-shell.git"
 
 #  Git clone destination directory
 local INSTALL_DIRECTORY="${HOME}/.satan-shell"
